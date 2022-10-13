93647fbc67bfa8fe8a0d8a33bc6dc87929d6a706
OnanThairu <thairuonan@gmail.com>
2022-10-13 07:31:32 +0300

    Keep user logged in.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import Pages from "./pages/Pages";
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetch("/me")
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((user) => setUser(user))
+        }
+      })
+  })
   return (
     <div className="App">
+      {
+        user ? <h2>Hello {user.username}</h2> : null
+      }
       <Pages />
     </div>
   );
